e87e03916643aea0e85eced4c947d1fa32cbbaf3
Orcablast <dbro0423@gmail.com>
2020-04-19 01:05:02 +0900

    0418 update - main_next&prev button

--- js/mainScrn.js
+++ js/mainScrn.js
@@ -1,20 +1,62 @@
 var imgNums = 8;
+var imgIndex = 0;
+var nums = [];
 
 $(function () {
-  imgSlide();
+  function getNums() {
+    for (var i = 1; i <= imgNums; i++) {
+      nums.push(i);
+    }
 
-  setInterval(slideAnimation, 5000);
-
-  function getImgNum() {
-    return Math.floor(Math.random() * imgNums) + 1;
+    nums.sort(function (a, b) {
+      return 0.5 - Math.random();
+    });
   }
 
+  getNums();
+
+  imgSlide();
+
+  interval = setInterval(slideAnimation, 5000);
+  timeOut = null;
+
   function imgSlide() {
-    $(".main_scrn img").attr("src", "./imgs/bg/" + getImgNum() + ".png");
+    if (imgIndex == imgNums || imgIndex < 0) {
+      imgIndex = 0;
+    }
+
+    console.log(imgIndex);
+    $(".main_scrn img").attr("src", "./imgs/bg/" + nums[imgIndex++] + ".png");
   }
 
   function slideAnimation() {
     $(".main_scrn img").animate({ opacity: "0" }, "slow", imgSlide);
     $(".main_scrn img").animate({ opacity: "0.65" }, "slow");
   }
+
+  $("#scrn_next").click(function () {
+    clearInterval(interval);
+    if (timeOut != null) {
+      clearTimeout(timeOut);
+    }
+
+    slideAnimation();
+    timeOut = setTimeout(function () {
+      interval = setInterval(slideAnimation, 5000);
+    }, 7000);
+  });
+
+  $("#scrn_prev").click(function () {
+    clearInterval(interval);
+    if (timeOut != null) {
+      clearTimeout(timeOut);
+    }
+
+    imgIndex = imgIndex - 2;
+
+    slideAnimation();
+    timeOut = setTimeout(function () {
+      interval = setInterval(slideAnimation, 5000);
+    }, 7000);
+  });
 });
